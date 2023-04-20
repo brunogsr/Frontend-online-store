2e1f1102ed8ca2f95cdda4554ac8c046ac2b7f5e
soltavares23 <solimartechti@gmail.com>
2023-04-20 12:13:05 -0300

    requisito 05

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 export default class Search extends Component {
   state = {
     inputSearch: '',
     categoriesList: [],
+    productsResults: [],
+    categoryId: '',
   };
 
   async componentDidMount() {
@@ -17,12 +19,28 @@ export default class Search extends Component {
     });
   }
 
-  onSearchButtonClick = () => {
+  onChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSearchClick = async () => {
+    const { categoryId, inputSearch } = this.state;
+    const getApi = await getProductsFromCategoryAndQuery(categoryId, inputSearch);
+    this.setState({
+      productsResults: getApi.results,
+    });
+  };
+
+  goToShoppingCart = () => {
     const { history } = this.props;
     history.push('/cart');
   };
 
   render() {
+    const { productsResults } = this.state;
     const {
       inputSearch,
       categoriesList,
@@ -30,16 +48,25 @@ export default class Search extends Component {
     return (
       <div>
         <input
+          data-testid="query-input"
           name="inputSearch"
           value={ inputSearch }
           type="text"
+          onChange={ this.onChange }
         />
+        <button
+          data-testid="query-button"
+          onClick={ this.handleSearchClick }
+
+        >
+          Pesquisar
+        </button>
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
         <button
           data-testid="shopping-cart-button"
-          onClick={ this.onSearchButtonClick }
+          onClick={ this.goToShoppingCart }
         >
           Cart Button
         </button>
@@ -53,14 +80,30 @@ export default class Search extends Component {
               >
                 { category.name }
                 <input
-                  id="categoria"
+                  id={ category.id }
                   type="radio"
-                  { ...category }
+                  name="categoryId"
+                  value={ category.id }
+                  onChange={ this.onChange }
+
                 />
               </label>
             ))
           }
         </div>
+        { productsResults.length === 0
+          ? (<h3>Nenhum produto foi encontrado</h3>)
+          : (
+            <div>
+
+              {productsResults.map((product, index) => (
+                <div key={ index }>
+                  <p data-testid="product">{ product.title }</p>
+                  <p>{ product.price }</p>
+                  <img src={ product.thumbnail } alt={ product.title } />
+                </div>
+              ))}
+            </div>)}
       </div>
     );
   }
