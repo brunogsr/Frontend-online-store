fc7fa024d02937be9b1a3048c041c24d447a2233
Thiagolad <thiago_lad@hotmail.com>
2023-04-22 16:06:53 -0300

    req10

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -12,30 +12,90 @@ class Cart extends React.Component {
     });
   }
 
+  dispatchCartUpdateEvent = () => {
+    const cartUpdateEvent = new Event('cartUpdate');
+    window.dispatchEvent(cartUpdateEvent);
+  };
+
+  updateValue = (id, action) => {
+    const DECREMENT = -1;
+    const arrayProductsCart = JSON.parse(localStorage.getItem('arrayProductsCart')) || [];
+    const product = arrayProductsCart.find((item) => item.id === id);
+
+    if (product.quantidade >= 2 || action !== 'decrease') {
+      product.quantidade += action === 'decrease' ? DECREMENT : 1;
+      localStorage.setItem('arrayProductsCart', JSON.stringify(arrayProductsCart));
+      this.setState({ arrayProductsCart });
+      this.dispatchCartUpdateEvent();
+    }
+  };
+
+  handleDeleteProduct = ({ target }) => {
+    const { arrayProductsCart } = this.state;
+    const newSavedProducts = arrayProductsCart.filter(({ id }) => id !== target.id);
+
+    this.setState({
+      arrayProductsCart: newSavedProducts,
+    });
+    localStorage.setItem('arrayProductsCart', JSON.stringify(newSavedProducts));
+  };
+
   render() {
     const { arrayProductsCart } = this.state;
     return (
       <div>
-        { arrayProductsCart.length > 0
-          ? (
-            arrayProductsCart.map(({ title, quantidade }, index) => (
-              <div key={ index }>
-                <p data-testid="shopping-cart-product-name">{ title }</p>
-                <br />
-                <p data-testid="shopping-cart-product-quantity">
-                  Quantidade:
-                  { quantidade }
-                </p>
-
-              </div>))
-          )
-          : (
-            <h1
-              data-testid="shopping-cart-empty-message"
+        {!arrayProductsCart.length ? (
+          <span
+            data-testid="shopping-cart-empty-message"
+          >
+            Seu carrinho está vazio
+          </span>
+        ) : (
+          arrayProductsCart.map(({ id, title, img, price, quantidade }) => (
+            <div
+              key={ id }
+              className="cart-product"
             >
-              Seu carrinho está vazio
-            </h1>)}
-
+              <img
+                src={ img }
+                alt={ title }
+              />
+              <span
+                data-testid="shopping-cart-product-name"
+              >
+                {title}
+              </span>
+              <button
+                onClick={ () => this.updateValue(id, 'decrease') }
+                data-testid="product-decrease-quantity"
+              >
+                -
+              </button>
+              <p
+                data-testid="shopping-cart-product-quantity"
+              >
+                {quantidade}
+              </p>
+              <button
+                onClick={ () => this.updateValue(id, 'increase') }
+                data-testid="product-increase-quantity"
+              >
+                +
+              </button>
+              <button
+                className="button-cart"
+                id={ id }
+                onClick={ this.handleDeleteProduct }
+                data-testid="remove-product"
+              >
+                Remover produto
+              </button>
+              <span>
+                {price}
+              </span>
+            </div>
+          ))
+        )}
       </div>
     );
   }
